f21dc4582159e19041c7867a34f19ef81c168d04
vitalii kovalenko <vitalii.kovalenko@nixs.com>
2023-04-11 09:35:44 +0300

    add class

--- admin-search-plugin.php
+++ admin-search-plugin.php
@@ -7,33 +7,69 @@
  * Author: KV
  */
 
+namespace SearchSlug;
+
 if ( ! defined('ABSPATH')) {
     exit;
 }
 
-add_filter('posts_search', 'search_slug', 10, 2);
 
-function search_slug($search, $wp_query) {
-    global $wpdb;
-    $result = $search;
+if ( ! class_exists('SearchSlug')) {
 
-    if  (!$wp_query->is_admin) {
-        return $result;
-    }
+    class SearchSlug
+    {
+        public function __construct() {
+            // Do nothing.
+        }
 
-    $slug = 'slug:';
-    $s = $wp_query->query_vars['s'];
+        /**
+         * @return void
+         */
+        public function initialize()
+        {
+            add_filter('posts_search', [$this, 'search_slug'], 10, 2);
+        }
 
-    if (!empty($s) && strripos($wp_query->query_vars['s'], $slug) !== false) {
-        $s = str_ireplace($slug, '',$s);
+        /**
+         * @param $search
+         * @param $wp_query
+         *
+         * @return mixed|string
+         */
+        public function search_slug($search, $wp_query) {
+            global $wpdb;
+            $result = $search;
 
-        if ($s !== '') {
-            $like  = '%' . $wpdb->esc_like( $s ) . '%';
-            $search = $wpdb->prepare( "{$wpdb->posts}.post_title LIKE %s", $like );
-            $result = " AND ({$search}) ";
+            if  (!$wp_query->is_admin) {
+                return $result;
+            }
+
+            $slug = 'slug:';
+            $s = $wp_query->query_vars['s'];
+
+            if (!empty($s) && strripos($wp_query->query_vars['s'], $slug) !== false) {
+                $s = str_ireplace($slug, '',$s);
+
+                if ($s !== '') {
+                    $like  = '%' . $wpdb->esc_like( $s ) . '%';
+                    $search = $wpdb->prepare( "{$wpdb->posts}.post_title LIKE %s", $like );
+                    $result = " AND ({$search}) ";
+                }
+            }
+
+            return $result;
         }
     }
 
-    return $result;
-}
+    function SearchSlug() {
+        global $searchSlug;
 
+        if ( ! isset( $searchSlug ) ) {
+            $searchSlug = new SearchSlug();
+            $searchSlug->initialize();
+        }
+        return $searchSlug;
+    }
+
+    SearchSlug();
+}
